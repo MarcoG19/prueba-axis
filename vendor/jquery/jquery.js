651fd520da6db24ecde3be0a47fddf3fb9e41826
Marco Gomez Rivero <gomezmarco195@gmail.com>
2020-11-07 21:58:51 -0500

    primer prueba

--- vendor/jquery/jquery.js
+++ vendor/jquery/jquery.js
@@ -11,6 +11,26 @@
  *
  * Date: 2019-05-01T21:04Z
  */
+
+
+$(".accordion-titulo").click(function(){
+		
+   var contenido=$(this).next(".accordion-content");
+			
+   if(contenido.css("display")=="none"){ //open		
+      contenido.slideDown(250);			
+      $(this).addClass("open");
+   }
+   else{ //close		
+      contenido.slideUp(250);
+      $(this).removeClass("open");	
+  }
+							
+});
+
+
+
+
 ( function( global, factory ) {
 
 	"use strict";
